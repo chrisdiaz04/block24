4fc61b3ac0736108d1a19ec3eba7a890e2ade294
chrisdiaz04 <diazchris27586@gmail.com>
2024-05-14 14:16:22 -0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
-  return 
-      <div>
-       
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppyList);
+
+  return (
+      <div>{ 
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+     }
       </div>
-  
+
+   )
 }
 
 export default App
